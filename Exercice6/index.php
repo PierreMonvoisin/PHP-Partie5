29c40dcd2d63dff44120cc4017e0640e9a31a515
PierreMonvoisin <monvoisin.pierre@gmail.com>
2019-12-09 15:51:27 +0100

    Partie en cours, Exercice 8 en cours

---
+++ Exercice6/index.php
@@ -0,0 +1,30 @@
+<?php
+$hautDeFrance['02'] = 'Aisne';
+$hautDeFrance['59'] = 'Nord';
+$hautDeFrance['60'] = 'Oise';
+$hautDeFrance['62'] = 'Pas-de-Calais';
+$hautDeFrance['80'] = 'Somme';
+?>
+<!DOCTYPE html>
+<html lang='fr' dir='ltr'>
+<head>
+  <title>Exercice 6</title>
+  <meta charset='UTF-8'>
+  <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+  <link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css' integrity='sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T' crossorigin='anonymous'>
+</head>
+
+<body class='container-fluid bg-secondary'>
+
+  <div class='row mt-5'>
+    <div class='jumbotron shadow-lg mx-auto text-center'>
+      <p><?= 'C\'est le '.$hautDeFrance['59'] ?></p>
+      <div class='text-right mt-1'>Un message de la direction</div>
+    </div>
+  </div>
+
+  <script src='https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js'></script>
+  <script src='https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js'></script>
+  <script src='https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js'></script>
+</body>
+</html>
---
+++ Exercice8/index.php
@@ -0,0 +1,26 @@
+<?php
+$mois = ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'];
+?>
+<!DOCTYPE html>
+<html lang='fr' dir='ltr'>
+<head>
+  <title>Exercice 7</title>
+  <meta charset='UTF-8'>
+  <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+  <link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css' integrity='sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T' crossorigin='anonymous'>
+</head>
+
+<body class='container-fluid bg-secondary'>
+
+  <div class='row mt-5'>
+    <div class='jumbotron shadow-lg mx-auto text-center'>
+      <p><?php print_r($mois) ?></p>
+      <div class='text-right mt-1'>Un message de la direction</div>
+    </div>
+  </div>
+
+  <script src='https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js'></script>
+  <script src='https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js'></script>
+  <script src='https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js'></script>
+</body>
+</html>

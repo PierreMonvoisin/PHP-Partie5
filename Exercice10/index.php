74cbcab985272f2df57383b71e83197357666578
PierreMonvoisin <monvoisin.pierre@gmail.com>
2019-12-10 09:58:22 +0100

    Changement du nombre de l'exerice dans le title

--- Exercice10/index.php
+++ Exercice10/index.php
@@ -8,7 +8,7 @@ $hautDeFrance['80'] = 'Somme';
 <!DOCTYPE html>
 <html lang='fr' dir='ltr'>
 <head>
-  <title>Exercice 9</title>
+  <title>Exercice 10</title>
   <meta charset='UTF-8'>
   <meta name='viewport' content='width=device-width, initial-scale=1.0'>
   <link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css' integrity='sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T' crossorigin='anonymous'>
